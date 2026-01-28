df1f357c0105294da3ac2e4abff7fbb4d54f1a29
Pavel Golovin <golovinp2014@mail.ru>
2026-01-28 19:20:56 +0300

    feat: payments bepaid

--- src/router.ts
+++ src/router.ts
@@ -17,6 +17,7 @@ import commentRoutes from './comment/comment';
 import higgsfieldRoutes from './higgsfield/higgsfield';
 import publicationRoutes from './publication/publication';
 import transactionRoutes from './transaction/transaction';
+import paymentRoutes from "./payment/payment";
 
 import { swaggerDefinition } from './swagger';
 
@@ -46,6 +47,7 @@ router.use('/gallery', galleryRoutes);
 router.use('/comments', commentRoutes);
 router.use('/higgsfield', higgsfieldRoutes);
 router.use('/transaction', transactionRoutes);
+router.use("/payment", paymentRoutes);
 router.use('/publications', publicationRoutes);
 
 export default router;
